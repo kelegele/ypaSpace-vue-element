a9921e11de498ac0c26350ab956b7eecbab7cf27
Kelegele <bskola@163.com>
2020-07-30 11:25:03 +0800

    list

--- src/views/private/index.vue
+++ src/views/private/index.vue
@@ -6,9 +6,36 @@
         <el-button v-waves style="margin-right: 20px;" class="filter-item" type="primary" icon="el-icon-search" @click="handleSearch">
           查找文件
         </el-button>
-        <el-button v-waves style="margin-right: 20px;" type="primary" icon="el-icon-upload" @click="handleUpload">
-          上传
-        </el-button>
+
+        <el-button v-waves style="margin-right: 20px;" icon="el-icon-upload" type="primary" @click="dialogVisible = true">上传</el-button>
+
+        <el-dialog title="上传文件" :visible.sync="dialogVisible">
+
+          <el-breadcrumb separator-class="el-icon-arrow-right" class="dialog-dir">
+            <el-breadcrumb-item>
+              {{ name }}
+            </el-breadcrumb-item>
+            <el-breadcrumb-item v-for="item in currentPaths" :key="item.index">
+              <el-link :underline="false" @click="onClickPath(item)"> {{ item.path }}</el-link>
+            </el-breadcrumb-item>
+          </el-breadcrumb>
+
+          <el-upload
+            class="upload"
+            action="https://jsonplaceholder.typicode.com/posts/"
+            :before-remove="beforeRemove"
+            show-file-list="false"
+            multiple
+            :limit="5"
+            :on-exceed="handleExceed"
+            :file-list="fileList"
+          >
+            <el-button v-waves style="margin-right: 20px;" type="primary" icon="el-icon-upload" @click="handleUpload">
+              上传
+            </el-button>
+          </el-upload>
+        </el-dialog>
+
         <el-button v-waves style="margin-right: 20px;" type="primary" icon="el-icon-download" @click="handleDownload">
           下载
         </el-button>
@@ -22,7 +49,7 @@
     <div class="container dir">
       <el-breadcrumb separator-class="el-icon-arrow-right">
         <el-breadcrumb-item>
-          {{ name }}
+          <el-link :underline="false" @click="onClickPath('/')"> {{ name }} </el-link>
         </el-breadcrumb-item>
         <el-breadcrumb-item v-for="item in currentPaths" :key="item.index">
           <el-link :underline="false" @click="onClickPath(item)"> {{ item.path }}</el-link>
@@ -36,7 +63,7 @@
           <div class="item" @click="onItemClick(item)">
             <i v-if="item.fileType === 'dir'" class="el-icon-folder" />
             <i v-else class="el-icon-document" />
-            <span><el-link> {{ item.fileName }}</el-link></span>
+            <span><el-link class="item-link"> {{ item.fileName }}</el-link></span>
           </div>
         </div>
       </div>
@@ -55,6 +82,7 @@ export default {
   directives: { waves },
   data() {
     return {
+      dialogVisible: false,
       searchInput: '',
       currentPaths: [],
       currentDir: [],
@@ -64,6 +92,7 @@ export default {
         name: undefined
       },
       itemSelected: [],
+      fileList: [],
       isCtrl: false,
       isShift: false
     }
@@ -82,7 +111,30 @@ export default {
     handleSearch() { },
     handleUpload() {},
     handleDownload() {},
-    handleNewFolder() { },
+    handleNewFolder() {
+      this.$prompt('请输入文件夹名称', '新建文件夹', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消'
+      }).then(({ value }) => {
+        this.$message({
+          type: 'success',
+          message: '新建文件夹: ' + value
+        })
+
+        console.log(value, this.currentPaths)
+        let path = '/'
+        this.currentPaths.forEach(element => {
+          path = path + '/' + element.path
+        })
+        path = path + '/' + value
+        console.log('NewFolder', path)
+      }).catch(() => {
+        this.$message({
+          type: 'info',
+          message: '取消输入'
+        })
+      })
+    },
     keyevent() {
       var that = this
       document.onkeydown = function(e) { // 按下键盘
@@ -108,12 +160,34 @@ export default {
     },
     onItemClick(item) {
       if (this.isCtrl) {
+        // TODO
         console.log('ctrl', item)
       } else {
-        console.log('no', item)
+        console.log('no ctrl', item)
+      }
+
+      if (item.fileType === 'dir') {
+        console.log('path', item.filePath)
+        this.getFiles(item.filePath)
       }
     },
-    onClickPath(item) {
+    onClickPath(path) {
+      // console.log(path)
+      let uPath = '/'
+
+      if (path === '/') {
+        return this.getFiles(uPath)
+      }
+
+      for (let i = 0; i < this.currentPaths.length; i++) {
+        uPath = uPath + '/' + this.currentPaths[i].path
+        // console.log(uPath)
+        if (path.index === this.currentPaths[i].index) {
+          // console.log(uPath)
+          break
+        }
+      }
+      this.getFiles(uPath)
     },
     getFiles(path) {
       console.log(path)
@@ -123,6 +197,13 @@ export default {
         console.log('res', this)
       }).catch(() => {
       })
+    },
+
+    handleExceed(files, fileList) {
+      this.$message.warning(`当前限制选择 3 个文件，本次选择了 ${files.length} 个文件，共选择了 ${files.length + fileList.length} 个文件`)
+    },
+    beforeRemove(file, fileList) {
+      return this.$confirm(`确定移除 ${file.name}？`)
     }
   }
 }
@@ -143,15 +224,28 @@ export default {
   right: 0;
 }
 
+.upload {
+  display: inline-block;
+}
+
 .dir {
   padding-left: 10px;
   position: relative;
+}
+
+.dialog-dir {
+  margin-bottom: 20px;
 }
 
 .grid {
   position: relative;
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(120px, 1fr));
+}
+
+.item-link{
+  word-wrap: break-word;
+  word-break: normal;
 }
 
 .item {
